f1ac3fe42b81a517312bebff6d32b6b38dba6247
bohdan20020808 <bohdan20020808@gmail.com>
2019-08-28 20:23:54 +0300

    Design

---
+++ NetLife/LogIn.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NetLife
+{
+    /// <summary>
+    /// Логика взаимодействия для LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        bool check = false;
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntranceClick(object sender, RoutedEventArgs e)
+        {
+            Main main = new Main();
+            main.Show();
+            this.Close();
+        }
+
+        private void btnCreateAccountClick(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Show();
+            this.Close();
+        }
+    }
+}
---
+++ NetLife/Registration.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NetLife
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateAccountClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your profile was created successfully");
+            LogIn log = new LogIn();
+            log.Show();
+            this.Close();
+        }
+
+        private void btnBackClick(object sender, RoutedEventArgs e)
+        {
+            LogIn log = new LogIn();
+            log.Show();
+            this.Close();
+        }
+    }
+}
