b94c5d94d135639af923a27423672bc18b3d055f
bohdan20020808 <bohdan20020808@gmail.com>
2019-08-28 22:34:26 +0300

    Entities

---
+++ NetLife/Entities/New.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLife.Entities
+{
+    [Table("tblNews")]
+    public class New
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, ForeignKey("IdOfUser")]
+        public int IdUser { get; set; }
+
+        [Required, StringLength(4000)]
+        public string TextOfNew { get; set; }
+
+        public virtual User IdOfUser { get; set; }
+    }
+}
---
+++ NetLife/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLife.Entities
+{
+    [Table("tblUsers")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(200)]
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        [Required]
+
+        public int Age { get; set; }
+
+        [Required, StringLength(10)]
+        public int Gender { get; set; }
+
+        [Required, StringLength(200)]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
